79c98fa9c13be48f645ae5aa43e3a65acd504968
Tuure Laurinolli <tuure@laurinolli.net>
2014-12-31 01:31:17 +0200

    Invoice formatting from Lauri

--- pik/util.py
+++ pik/util.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8
 import datetime as dt
+import billing
+
 
 class Period(object):
     def __init__(self, start, end):
@@ -15,3 +18,43 @@ class Period(object):
 
     def __contains__(self, date):
         return self.start <= date and date <= self.end
+
+def format_invoice(invoice, additional_details=None):
+
+    dateformat = "%d.%m.%Y"
+    spacer = "---------------------------"
+    due_in = dt.timedelta(14)
+
+    if not additional_details:
+        additional_details = "Laskussa on huomioitu lennot ja tilitapahtumat 2014 loppuun saakka."
+
+    total_price = sum(l.price for l in invoice.lines)
+
+    ret = \
+    u"PIK ry jäsenlaskutus\n" + spacer + "\n" + \
+    u"Laskun päivämäärä: " + invoice.date.strftime(dateformat) + "\n\n" + \
+    u"Saaja: Polyteknikkojen Ilmailukerho ry\n" + \
+    u"Saajan tilinumero: FI24 1309 3000 1124 58 (Nordea)\n\n" + \
+    u"Viitenumero (PIK-viite): " + invoice.account_id + "\n" + \
+    u"Laskun eräpäivä: " + (invoice.date + due_in).strftime(dateformat) + "\n\n" + \
+    u"Maksettavaa: %.2f EUR" % (total_price) + "\n" + spacer + "\n\n"
+
+    ret += additional_details + "\n\n"
+
+    ret += u"Laskun erittely: \n\n"
+
+    for line in sorted(invoice.lines, key=lambda line: line.date):
+        if line.price == 0:
+            continue
+        ret += "%s %s:  %.2f" % (line.date.strftime(dateformat), line.item, line.price) +"\n"
+    ret += "\n"
+
+    ret += u"Lasku sisältää myös seuraavat tapahtumat (à 0 EUR):\n\n"
+
+    for line in sorted(invoice.lines, key=lambda line: line.date):
+        if not line.price == 0:
+            continue
+
+        ret += u"%s %s" % (line.date.strftime(dateformat), line.item) +"\n"
+
+    return ret
